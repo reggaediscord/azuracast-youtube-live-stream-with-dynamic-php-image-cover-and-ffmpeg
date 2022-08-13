c7aa0c7a75ae2c081dc1f39937b37fc42aa8eb07
Reggae Discord <david.simonnard@protonmail.com>
2022-08-13 12:37:24 +0200

    Update radio.sh

--- radio.sh
+++ radio.sh
@@ -7,7 +7,7 @@ VIDEO_SOURCE="/home/radio/bg.gif"
 AUDIO_SOURCE="http://XXX.XXX.XXX.XXX:8000/radio.mp3"
 
 nice -n 19 ffmpeg \
-    -re -stream_loop -1 -i "$VIDEO_SOURCE" -pattern_type glob -stream_loop -1 -r 1 -re -i '/home/radio/cove*.png' -filter_complex '[0:v][1:v] overlay' \
+    -re -stream_loop -1 -i "$VIDEO_SOURCE" -pattern_type glob -stream_loop -1 -framerate 1 -re -i '/home/radio/cove*.png' -filter_complex '[0:v][1:v] overlay' \
     -thread_queue_size 512 -i "$AUDIO_SOURCE" \
     -map 0:v:0 -map 1:a:0 \
     -map_metadata:g 1:g \
